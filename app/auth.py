9058ce531a0965182ca8c0c90b18af48f136ee9f
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-08-15 14:13:34 +0100

    formatted black

--- app/auth.py
+++ app/auth.py
@@ -1,37 +1,52 @@
-from flask import Flask, request, send_from_directory, render_template, Blueprint, redirect, url_for
+from flask import (
+    Flask,
+    request,
+    send_from_directory,
+    render_template,
+    Blueprint,
+    redirect,
+    url_for,
+)
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login')
+
+@auth.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
-@auth.route('/register')
+
+@auth.route("/register")
 def register():
-    return render_template('register.html')
+    return render_template("register.html")
 
-@auth.route('/logout')
+
+@auth.route("/logout")
 def logout():
-    return 'Logout'
+    return "Logout"
 
-@auth.route('/register', methods=['POST'])
+
+@auth.route("/register", methods=["POST"])
 def register_post():
-    email = request.form('email')
-    name = request.form('name')
-    password = request.form('password')
+    email = request.form("email")
+    name = request.form("name")
+    password = request.form("password")
 
-    
     user = User.query.filter_by(email=email).first()
 
     if user:
-        return redirect(url_for('auth.register'))
+        return redirect(url_for("auth.register"))
 
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(
+        email=email,
+        name=name,
+        password=generate_password_hash(password, method="sha256"),
+    )
 
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('auth.login'))
+    return redirect(url_for("auth.login"))
--- hello-world-with-flask/main.py
+++ hello-world-with-flask/main.py
@@ -1,9 +1,4 @@
-from flask import (
-    Flask,
-    request,
-    send_from_directory,
-    render_template
-)
+from flask import Flask, request, send_from_directory, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,28 +7,32 @@ app = Flask(__name__)
 CORS(app)
 
 # Example of a simple page, here at out homepage, as signified by the "/" path
-@app.route('/')
+@app.route("/")
 def hello_world():
     return render_template("login.html")
 
+
 # Example of using parameters in the url, which may be used for finding entities
-@app.route('/hello')
+@app.route("/hello")
 def give_name():
-    name = request.args.get('name')
+    name = request.args.get("name")
     if not name:
         return "Hi! add '?name=<name>' to the url to get a funky message!"
     else:
         return f"Hi, {name}!"
 
-# Examples of getting data from directories using Flask
-@app.route('/geojson/<path:path>')
-def getGeojson(path):
-    return send_from_directory('geojson', path)
 
-@app.route('/reporting_entities/<path:path>')
+# Examples of getting data from directories using Flask
+@app.route("/geojson/<path:path>")
+def getGeojson(path):
+    return send_from_directory("geojson", path)
+
+
+@app.route("/reporting_entities/<path:path>")
 def getDatajson(path):
-    return send_from_directory('reporting_entities', path)
+    return send_from_directory("reporting_entities", path)
+
 
 # Run this file to start the *development* server
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=8080)
