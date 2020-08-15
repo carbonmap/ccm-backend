8431b69fb2816b297c5749d00af49c64a5d07210
Jeevs09 <jeevan.bhoot@yahoo.com>
2020-08-15 17:55:42 +0100

    Basic login + registration working with database.

--- app/main.py
+++ app/main.py
@@ -1,22 +1,13 @@
-from flask import Flask, request, send_from_directory, render_template, Blueprint
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from . import db
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 main = Blueprint("main", __name__)
-
 
 @main.route("/")
 def index():
     return render_template("index.html")
 
-
 @main.route("/profile")
+@login_required
 def profile():
-    return render_template("profile.html")
-
-
-# Example of a simple page, here at out homepage, as signified by the "/" path
-@app.route("/")
-def hello_world():
-    return render_template("login.html")
+    return render_template("profile.html", name=current_user.name)
