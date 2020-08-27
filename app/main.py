d1e4879e2e96ed45b20f0a396242729648e322bb
PhillipAColes <phillipanthonycoles@gmail.com>
2020-08-28 00:39:54 +0100

    added functionality to list user subentites in profile, and re-added name attributes into templates

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from data_access_functions import user_map_dashboard
 
 main = Blueprint("main", __name__)
 
@@ -10,4 +11,8 @@ def index():
 @main.route("/profile")
 @login_required
 def profile():
-    return render_template("profile.html", name=current_user.name)
+    user_email_domain = \
+        user_map_dashboard.get_user_email_domain(current_user.email)
+    user_subentities = user_map_dashboard.get_subentities(user_email_domain)
+    return render_template("profile.html", name=current_user.name,
+                          subentities=user_subentities)
