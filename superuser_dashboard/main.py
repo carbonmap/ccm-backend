2c0fd268788a7fb1265d3d0545ff660766054206
Henry Worrall <61287647+henry-worrall@users.noreply.github.com>
2020-08-31 22:09:38 +0100

    Print files in folders to html
    List is hardcoded in with html loop linked to list in main.py. This will need changing so that the list is called in JS and page doesn't have to be refreshed when a change occurs.

--- superuser_dashboard/main.py
+++ superuser_dashboard/main.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import os, os.path
 
 app = Flask(__name__)
 
+limbo_path = "superuser_dashboard/limbo_state/"
+confirmed_path = "superuser_dashboard/confirmed_state/"
+file_name = "some_text.txt"
+
+limbo_list = os.listdir(limbo_path) #list of all the files limbo folder
+confirmed_list = os.listdir(confirmed_path) #list of all the files limbo folder
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", limbo_list=limbo_list, confirmed_list=confirmed_list)
 
-@app.route('/SomeFunction')
-def SomeFunction(): 
+
+@app.route("/SomeFunction")
+def SomeFunction():
     # function checks which folder the file is in, then moves it to the other folder
-    
-    if os.path.isfile('superuser_dashboard/limbo_state/some_text.txt') == True:
-        os.rename('superuser_dashboard/limbo_state/some_text.txt', 'superuser_dashboard/confirmed_state/some_text.txt')
-        
-    elif os.path.isfile('superuser_dashboard/confirmed_state/some_text.txt') == True:
-        os.rename('superuser_dashboard/confirmed_state/some_text.txt', 'superuser_dashboard/limbo_state/some_text.txt')
-    
-    print('The text file has been moved')
-    return "Nothing"
+
+    if os.path.isfile(os.path.join(limbo_path, file_name)) == True:
+        os.rename(
+            os.path.join(limbo_path, file_name),
+            os.path.join(confirmed_path, file_name),
+        )
+
+    elif os.path.isfile(os.path.join(confirmed_path, file_name)) == True:
+        os.rename(
+            os.path.join(confirmed_path, file_name),
+            os.path.join(limbo_path, file_name),
+        )
+
+    print("The text file has been moved")
+    return "None"
 
 if __name__ == "__main__":
     app.run(debug=True)
