7534c2f79928ad08c53b9cde29cb5d7ff7c1a62d
Henry Worrall <61287647+henry-worrall@users.noreply.github.com>
2020-09-22 15:28:04 +0100

    Changes to move individual files on front end

--- app/superuser_dash.py
+++ app/superuser_dash.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 import os, os.path
 import shutil
 
@@ -14,35 +14,52 @@ confirmed_list = os.listdir(confirmed_path)  # list of all the files limbo folde
 
 @superuser_dashboard.route("/superuser_dashboard")
 def super_dash():
-    return render_template(
-        "super_dash.html"
-    )
+    return render_template("super_dash.html")
 
 
-@superuser_dashboard.route("/showfiles", methods=['POST'])
+@superuser_dashboard.route("/showfiles", methods=["POST"])
 def show_files():
-    limbo_list=os.listdir(limbo_path)
-    confirmed_list=os.listdir(confirmed_path)
-    data = {'limbo' : limbo_list, 'confirmed' : confirmed_list}
+    limbo_list = os.listdir(limbo_path)
+    confirmed_list = os.listdir(confirmed_path)
+    data = {"limbo": limbo_list, "confirmed": confirmed_list}
     return jsonify(data)
 
 
-@superuser_dashboard.route("/movefile", methods=['POST'])
+@superuser_dashboard.route("/movefile", methods=["GET", "POST"])
 def Move():
-    # function checks which folder the file is in, then moves it to the other folder
 
-    if os.path.isfile(os.path.join(limbo_path, file_name)) == True:
-        shutil.move(
-            os.path.join(limbo_path, file_name),
-            os.path.join(confirmed_path, file_name),
-        )
+    if request.method == "GET":  # get request left for backend page functionality
+        if os.path.isfile(os.path.join(limbo_path, file_name)) == True:
+            shutil.move(
+                os.path.join(limbo_path, file_name),
+                os.path.join(confirmed_path, file_name),
+            )
 
-    elif os.path.isfile(os.path.join(confirmed_path, file_name)) == True:
-        shutil.move(
-            os.path.join(confirmed_path, file_name),
-            os.path.join(limbo_path, file_name),
-        )
+        elif os.path.isfile(os.path.join(confirmed_path, file_name)) == True:
+            shutil.move(
+                os.path.join(confirmed_path, file_name),
+                os.path.join(limbo_path, file_name),
+            )
 
-    print("The text file has been moved")
+    if request.method == "POST":  # request used in frontend
+
+        name = request.json[
+            "name"
+        ]  # sets 'name' object to the 'name' of file in json pushed from frontend
+
+        # logic checks which folder the file (with name == name) is in, then moves it to the other folder
+        if os.path.isfile(os.path.join(limbo_path, name)) == True:
+            shutil.move(
+                os.path.join(limbo_path, name),
+                os.path.join(confirmed_path, name),
+            )
+
+        elif os.path.isfile(os.path.join(confirmed_path, name)) == True:
+            shutil.move(
+                os.path.join(confirmed_path, name),
+                os.path.join(limbo_path, name),
+            )
+
+        print("The text file has been moved")
 
     return show_files()
