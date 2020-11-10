2b39ae09769203381f4dfdad1f41686501ba2cee
Chris Pointon <cpointon@influencenetwork.com>
2020-11-10 23:37:50 +0000

    Make path to .env explicit

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from flask_dotenv import DotEnv
+from os import path
 
 class Config(object):
     DEBUG = False
@@ -6,7 +7,8 @@ class Config(object):
     @classmethod
     def init_app(self, app):
         env = DotEnv()
-        env.init_app(app, verbose_mode=True)
+        env_file_path = path.join(path.dirname(path.abspath(__file__)), '.env')
+        env.init_app(app, env_file=env_file_path, verbose_mode=True)
 
 class ProductionConfig(Config):
     pass
